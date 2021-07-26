563587be8e9e03ef23e4a3a37ba64c1c07064e21
katskurka <kati.skurka@gmail.com>
2021-07-25 20:49:18 -0400

    tweaked filter function and added a check to return NaN, all tests pass

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
-// stringified numbers should be treated as numbers
-// null should be treated as zero
-// undefined values should be ignored
-// empty string values should be ignored
-// non-numeric values should be ignored
-
 function calculate(calculationSteps) {
-  // const operators = ['+', '-', '*', '/']
+  let operators = ['+', '-', '/', '*']
 
-  // for (let i = 0; i < calculationSteps.length; i++) {
-  //   if (calculationSteps.includes(operators)) {
-  //     return true
-  //   }
-  //   else {
-  //     return NaN
-  //   }
-  // }
-  calculationSteps.map(calculationStep => {
+  calculationSteps = calculationSteps.map(calculationStep => {
     if (calculationStep === null) {
       return 0
     }
@@ -23,22 +9,18 @@ function calculate(calculationSteps) {
       return calculationStep
     }
   })
-  // calculationSteps.map(calculationStep => {
-  //   if (calculationStep === isNaN(parseInt(calculationStep))) {
-  //     return ''
-  //   }
-  //   else {
-  //     return calculationStep
-  //   }
-  // })
   let cleanedValues = calculationSteps.filter(calculationStep => {
-    if (calculationStep === '' || calculationStep === undefined || calculationStep ===
-    isNaN(parseInt(calculationStep))) {
+    if (calculationStep === '' || calculationStep === undefined || (isNaN(parseInt(calculationStep)) &&
+    !operators.includes(calculationStep))) {
       return false
     }
 
     return true
   })
+
+  if (cleanedValues.length !== 3) {
+    return NaN
+  }
 
   return calculations(cleanedValues[0], cleanedValues[1], cleanedValues[2])
 }
@@ -53,4 +35,3 @@ function calculations(numberOne, operator, numberTwo) {
 }
 
 module.exports = calculate
-
